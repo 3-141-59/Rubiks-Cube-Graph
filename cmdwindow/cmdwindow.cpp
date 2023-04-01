b7f06185166e89f3e15d3d7fbfa92dc142f9429b
3-141-59 <129062223+3-141-59@users.noreply.github.com>
2023-04-01 18:25:22 +0000

    Initial commit (for file structure)

---
+++ cmdwindow/cmdwindow.cpp
@@ -0,0 +1,20 @@
+/*
+ * This cpp file will send and recieve data to the main file
+ * to enable multiple windows being used for the same program
+ * in an easier fashion to most ways you would typically do this.
+ *
+ * We'll use temporary files to send get the data almost acting
+ * like "cache" files but some files won't be retained.
+ *
+ * Instead of using header files for this, using either bash/shell
+ * script we can easily open two windows however this whole system
+ * may end up being used with header files to act as a lib if I
+ * end up redoing this for the purpose of being a library.
+ * */
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
